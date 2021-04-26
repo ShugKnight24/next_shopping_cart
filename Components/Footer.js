9fa7063636dc91779044fe5bddc455546d0511a1
Shugmi Shumunov <SShumunov@gmail.com>
2021-04-26 13:19:48 -0400

    Create Footer component

---
+++ Components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer(){
+	return(
+		<div className="footer-container">
+			<footer>
+				Copyright 2021 &copy; Shugmi Shumunov All Rights Reserved
+			</footer>
+		</div>
+	)
+}
