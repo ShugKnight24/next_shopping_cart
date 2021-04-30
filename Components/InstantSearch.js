2d5b7a5c89249c43c42ab28d8dc6dbd69da8c4bd
Shugmi Shumunov <SShumunov@gmail.com>
2021-04-30 09:40:24 -0400

    Build instant search hit

--- Components/InstantSearch.js
+++ Components/InstantSearch.js
@@ -1,9 +1,15 @@
-import { useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import Link from 'next/link';
+import { CartContext } from '../context/CartProvider';
 
 export function InstantSearch(){
 	const apiKey = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY;
 	const appId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID;
 	const indexName = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME;
+
+	const { state, dispatch } = useContext(CartContext);
+	const [showHits, setShowHits] = useState(false);
+	const [hitList, setHitList] = useState([]);
 
 	const url = `https://${ appId }-dsn.algolia.net/1/indexes/${ indexName }`;
 	/*
@@ -24,7 +30,51 @@ export function InstantSearch(){
 		}
 	})
 	.then(response => response.json())
-	.then(data => console.log(data));
+	.then(data => buildSearch(data.hits));
+
+	function handleButtonClick(event, productId){
+		event.stopPropagation();
+		dispatch({ 
+			type: 'ADD_ITEM',
+			payload: {
+				productId
+			}
+		});
+	}
+
+	// update # available from inventory state
+	const buildSearch = (algoliaResponse) => {
+		const hitsHTML = algoliaResponse.map(product => {
+			const disabledButton = product.available === 0 ? true : false;
+			return(
+				<li 
+					className="hit-container"
+					key={ product.itemid }
+				>
+					<Link href={`/products/${ product.itemid.toString() }`}>
+						<a>
+							<div className="hit-content">
+								<img
+									className="hit-image"
+									src={ product.image }
+									alt={`${ product.productName } made by ${ product.manufacturer }`}
+								/>
+								<div className="name-manufacturer">
+									<h2>{ product.productName }</h2>
+									<h3>Made By: { product.manufacturer }</h3>
+								</div>
+								<button 
+									className={`button add-cart-button ${ disabledButton ? 'disabled' : '' }`}
+									onClick={ (event) => handleButtonClick(event,  product.itemid) }
+								>Add To Cart</button>
+							</div>
+						</a>
+					</Link>
+				</li>
+			);
+		});
+		setHitList(hitsHTML);
+	}
 
 	useEffect(() => {
 		fetchAlgoliaData();
@@ -32,7 +82,27 @@ export function InstantSearch(){
 
 	return(
 		<div className="instant-search-container">
-			<div>This is InstantSearch</div>
+			<input
+				className="instant-search"
+				type="text"
+				placeholder="Search Products"
+				onClick={
+					() => setShowHits(true)
+				}
+			/>
+			<ul className={`instant-list ${ showHits ? 'populated' : '' }`}>
+				{
+					showHits
+					? hitList.map(hit => <>{hit}</>)
+					: (
+						<>
+							<li><a>Find the products you want</a></li>
+							<li><a>Filter by name and manufacturer</a></li>
+						</>
+					)
+				}
+				
+			</ul>
 		</div>
 	);
 }
