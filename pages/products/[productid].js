2ed74d56774965c04a0ab5fb7780d93ccfb7943c
Shugmi Shumunov <SShumunov@gmail.com>
2021-05-02 12:11:00 -0400

    Update subproduct pages
    - Fix available
    - Fix disabled button
    - Add link to ProductsPage

--- pages/products/[productid].js
+++ pages/products/[productid].js
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 import productList from '../../data/items.json';
 import { CartContext } from '../../context/CartProvider';
 
@@ -33,13 +34,15 @@ export const getStaticProps = (context) => {
 
 export default function ProductID({ currentProduct }){
 	const { state, dispatch } = useContext(CartContext);
-	const disabledButton = currentProduct.available === 0 ? true : false;
+	const { inventory } = state;
+	const currentItem = inventory.find((product) => product.itemid === currentProduct.itemid)
+	const disabledButton = currentItem.available === 0 ? true : false;
 
 	// TODO: Finish Add to Cart functionality
-	function handleSubmit(event){
-		event.preventDefault();
-		console.log(event);
-	}
+	// function handleSubmit(event){
+	// 	event.preventDefault();
+	// 	console.log(event);
+	// }
 
 	// Long Term Goal
 	// Product Added Modal
@@ -53,55 +56,54 @@ export default function ProductID({ currentProduct }){
 			</Head>
 			<div className="product-page">
 				<div className="product-container">
-					{/* Link back to all products page */}
-					{
-						<div
-						className="product"
-						key={ currentProduct.itemid }
-						>
-							<div className="page-header">
-								<h1>{ currentProduct.productName } Details</h1>
-								<h2>Manufactured By: { currentProduct.manufacturer }</h2>
-							</div>
-							<img 
-								src={ currentProduct.image }
-								alt={`${ currentProduct.productName } by ${ currentProduct.manufacturer }`}
-								/>
-							<p className="product-description"></p>
-							<p className="product-Price">Price: ${ currentProduct.price }</p>
-							{/* Refactor available */}
-							{/* <p className="product-quantity">Currently Available: { currentProduct.available }</p> */}
-							<button
-								className={`button add-cart-button ${ disabledButton ? 'disabled' : '' }`}
-								onClick={ () => dispatch({ 
-									type: 'ADD_ITEM',
-									payload: {
-										productId: currentProduct.itemid
-									}
-								}) }
-							>Add To Cart</button>
-							{/* <form
-								className="add-to-cart-form"
-								onSubmit={ (event) => handleSubmit(event) }
-							>
-								<label htmlFor="quantity">Quantity:</label>
-								<input
-									id="quantity"
-									className="quantity-input"
-									type="number"
-									name="quantity"
-									value="1"
-									min="0"
-									max={ currentProduct.available }
-									onChange={ (event) => updateQuantity(event) }
-								/>
-								<button 
-									className="button add-cart-button"
-									type="submit"
-								>Add To Cart</button>
-							</form> */}
+				<Link href="/products"><a>All Products</a></Link>
+				{
+					<div
+					className="product"
+					key={ currentProduct.itemid }
+					>
+						<div className="page-header">
+							<h1>{ currentProduct.productName } Details</h1>
+							<h2>Manufactured By: { currentProduct.manufacturer }</h2>
 						</div>
-					}
+						<img 
+							src={ currentProduct.image }
+							alt={`${ currentProduct.productName } by ${ currentProduct.manufacturer }`}
+							/>
+						<p className="product-description"></p>
+						<p className="product-price">Price: ${ currentProduct.price }</p>
+						<p className="product-quantity">Currently Available: { currentItem.available }</p>
+						<button
+							className={`button add-cart-button ${ disabledButton ? 'disabled' : '' }`}
+							onClick={ () => dispatch({ 
+								type: 'ADD_ITEM',
+								payload: {
+									productId: currentProduct.itemid
+								}
+							}) }
+						>Add To Cart</button>
+						{/* <form
+							className="add-to-cart-form"
+							onSubmit={ (event) => handleSubmit(event) }
+						>
+							<label htmlFor="quantity">Quantity:</label>
+							<input
+								id="quantity"
+								className="quantity-input"
+								type="number"
+								name="quantity"
+								value="1"
+								min="0"
+								max={ currentProduct.available }
+								onChange={ (event) => updateQuantity(event) }
+							/>
+							<button 
+								className="button add-cart-button"
+								type="submit"
+							>Add To Cart</button>
+						</form> */}
+					</div>
+				}
 				</div>
 			</div>
 		</>
