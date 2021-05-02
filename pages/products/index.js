389d68e45752ad1013f401e1498cef3f08986e80
Shugmi Shumunov <SShumunov@gmail.com>
2021-05-02 12:09:53 -0400

    Make Products its own component
    Seperate from ProductsPage

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,27 +1,7 @@
-import { useContext, useState } from 'react';
 import Head from 'next/head';
-import Link from 'next/link';
-import { CartContext } from '../../context/CartProvider';
-import { InstantSearch } from '../../Components/InstantSearch/InstantSearch';
+import { Products } from '../../Components/Products';
 
-export default function Products(){
-	const { state, dispatch } = useContext(CartContext);
-	const { inventory } = state;
-	const [showHitsClosed, setShowHitsClosed] = useState(null);
-
-	function handleClick(event){
-		const element = document.querySelector('.instant-search');
-
-		// work around
-		// functional components won't rerender if state is reset to same
-		if (event.target !== element && !element.contains(event.target)) {
-			setShowHitsClosed(false);
-			setTimeout(() => {
-				setShowHitsClosed(null);
-			}, 500)
-		};
-	}
-
+export default function ProductsPage(){
 	return(
 		<>
 			<Head>
@@ -31,54 +11,7 @@ export default function Products(){
 				<div className="page-header">
 					<h1>All Products</h1>
 				</div>
-				<div className="products-container"
-					onClick={
-						(event) => handleClick(event) 
-					}
-				>
-					<InstantSearch 
-						showHitsClosed={ showHitsClosed }
-					/>
-					{
-						inventory &&
-						inventory.map(product => {
-							const disabledButton = product.available === 0 ? true : false;
-							return(
-								<div
-									className="product"
-									key={ product.itemid }
-								>
-									<img 
-										src={ product.image }
-										alt={`${ product.productName } made by ${ product.manufacturer }`}
-									/>
-									<h2>{ product.productName }</h2>
-									<h3>Manufactured By: { product.manufacturer }</h3>
-									<div className="product-info">
-										{/* TODO:// Trim description Text `...Read More` => Head to Specific product */}
-										<p className="product-description">{ product.description }</p>
-										<p className="product-Price">Price: ${ product.price }</p>
-										<p className="product-quantity">Currently Available: { product.available }</p>
-										<div className="product-actions">
-											<button className="button more-info-button">
-												<Link href={`/products/${ product.itemid.toString() }`}><a>More Info</a></Link>
-											</button>
-											<button
-												className={`button add-cart-button ${ disabledButton ? 'disabled' : '' }`}
-												onClick={ () => dispatch({ 
-													type: 'ADD_ITEM',
-													payload: {
-														productId: product.itemid
-													}
-												}) }
-											>Add To Cart</button>
-										</div>
-									</div>
-								</div>
-							)
-						})
-					}
-				</div>
+				<Products />
 			</div>
 		</>
 	);
