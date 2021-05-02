f21f428db8eb4eff3904c4ab915b1147d8abd29e
Shugmi Shumunov <SShumunov@gmail.com>
2021-05-02 15:25:40 -0400

    Refactor initial version of product recommendation popup

--- pages/products/index.js
+++ pages/products/index.js
@@ -6,9 +6,10 @@ import { CartContext } from '../../context/CartProvider';
 import { ModalContext } from '../../context/ModalProvider';
 import { Modal } from '../../Components/Modal';
 import { getCurrentItem } from '../../utils/getItem';
+import { formatCurrency } from '../../utils/cartUtils';
 
 export default function ProductsPage(){
-	const { showModal } = useContext(ModalContext);
+	const { showModal, setShowModal } = useContext(ModalContext);
 	const { state, dispatch } = useContext(CartContext)
 	const { inventory } = state;
 
@@ -38,36 +39,40 @@ export default function ProductsPage(){
 					showModal ? (
 						<Modal>
 							<div className="modal-body">
-								You selected { selectedProduct }, it pairs well with { recommendedProduct }. want this too?
+								You selected { selectedProduct }, it pairs well with { recommendedProduct }. Do you want to add this product to your cart as well?
 
-								<h2>{ recommendedProduct } Details</h2>
+								<Link href={`/products/${ recItem.itemid.toString() }`}>
+									<a>
+										<h2>{ recommendedProduct } Details</h2>
+									</a>
+								</Link>
 								{ recItem &&
 									(
-										<Link href={`/products/${ recItem.itemid.toString() }`}>
-										<a>
-											<div className="hit-content">
-												<img
-													className="hit-image"
-													src={ recItem.image }
-													alt={`${ recItem.productName } made by ${ recItem.manufacturer }`}
-												/>
-												<div className="name-manufacturer">
-													<h2>{ recItem.productName }</h2>
-													<h3>Made By: { recItem.manufacturer }</h3>
-													<p>Available: { recItem.available }</p>
-												</div>
-												<button 
-													className={`button add-cart-button`}
-													onClick={ () => dispatch({ 
+										<div className="hit-content">
+											<img
+												className="hit-image"
+												src={ recItem.image }
+												alt={`${ recItem.productName } made by ${ recItem.manufacturer }`}
+											/>
+											<div className="product-info">
+												<h3>Made By: { recItem.manufacturer }</h3>
+												<p>{ recItem.description }</p>
+												<p>Available: { recItem.available }</p>
+												<p>Price: { formatCurrency(recItem.price) }</p>
+											</div>
+											<button 
+												className={`button add-cart-button`}
+												onClick={ () => {
+													dispatch({ 
 														type: 'ADD_ITEM',
 														payload: {
 															productId: recItem.itemid
 														}
-													}) }
-												>Add To Cart</button>
-											</div>
-										</a>
-									</Link>
+													});
+													setShowModal(false);
+												}}
+											>Add To Cart</button>
+										</div>
 									)
 								}
 							</div>
