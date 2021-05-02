3d37940d685a9a5f56759ba0bf9ee4a0c513eaa9
Shugmi Shumunov <SShumunov@gmail.com>
2021-05-02 12:12:43 -0400

    Add Products component to Home

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,17 @@
 import { useContext, useEffect, useState } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-import { CartContext } from '../context/CartProvider';
-import { InstantSearch } from '../Components/InstantSearch/InstantSearch';
+import { Products } from '../Components/Products';
 import { ModalContext } from '../context/ModalProvider';
 import { Modal } from '../Components/Modal';
 
 export default function Home() {
-	const { state, dispatch } = useContext(CartContext);
-	const { inventory } = state;
-	const [showHitsClosed, setShowHitsClosed] = useState(null);
 	const { showModal } = useContext(ModalContext);
 
 	const [selectedProduct, setSelectedProduct] = useState('');
 	const [recommendedProduct, setRecommendedProduct] = useState('');
 	const [recItem, setRecItem] = useState(null);
 
-	function handleClick(event){
-		const element = document.querySelector('.instant-search');
-
-		// work around
-		// functional components won't rerender if state is reset to same
-		if (event.target !== element && !element.contains(event.target)) {
-			setShowHitsClosed(false);
-			setTimeout(() => {
-				setShowHitsClosed(null);
-			}, 500)
-		};
-	}
 
 	useEffect(() => {
 		const currentItem = inventory.find(product => product.itemid === recommendedProduct);
@@ -44,64 +28,10 @@ export default function Home() {
 					<h1>Home Page</h1>
 					<h2>Our Most Popular Products</h2>
 				</div>
-				<div className="products-container"
-					onClick={
-						(event) => handleClick(event) 
-					}
-				>
-					<InstantSearch 
-						showHitsClosed={ showHitsClosed }
-						setSelectedProduct={ setSelectedProduct }
-						setRecommendedProduct={ setRecommendedProduct }
-					/>
-				{
-					inventory &&
-					inventory.map(product => {
-						const disabledButton = product.available === 0 ? true : false;
-						return(
-							<div
-								className="product"
-								key={ product.itemid }
-							>
-								<img 
-									src={ product.image }
-									alt={`${ product.productName } made by ${ product.manufacturer }`}
-								/>
-								<h2>{ product.productName }</h2>
-								<h3>Manufactured By: { product.manufacturer }</h3>
-								<div className="product-info">
-									{/* TODO:// Trim description Text `...Read More` */}
-									<p className="product-description">{ product.description }</p>
-									<p className="product-Price">Price: ${ product.price }</p>
-									{/* 
-										TODO:// Reduce this number as items are added to cart... 
-										Can't add more than currently available
-									*/}
-									<p className="product-quantity">Currently Available: { product.available }</p>
-									<div className="product-actions">
-										<Link href={`/products/${ product.itemid.toString() }`}>
-											<button className="button more-info-button">
-												<a>More Info</a>
-											</button>
-										</Link>
-										{/* TODO:// Add to cart functionality */}
-										<button 
-											className={`button add-cart-button ${ disabledButton ? 'disabled' : '' }`}
-											onClick={ () => dispatch({ 
-												type: 'ADD_ITEM',
-												payload: {
-													productId: product.itemid
-												}
-											}) }
-										>Add To Cart</button>
-									</div>
-								</div>
-							</div>
-						)
-					})
-				}
-					<Link href='/products'><a className="all-products-link">See All Products</a></Link>
-				</div>
+				<Products />
+				<Link href='/products'>
+					<a className="all-products-link">See All Products</a>
+				</Link>
 				{ 
 					showModal ? (
 						<Modal>
