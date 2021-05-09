96f0675fc45cc840e34119f35a26c68df8e03431
Shugmi Shumunov <SShumunov@gmail.com>
2021-05-09 13:22:50 -0400

    Refactor cart styles

--- pages/cart.js
+++ pages/cart.js
@@ -33,80 +33,95 @@ export default function Cart(){
 			</Head>
 			<div className="cart-container">
 				<div className="page-header">
-					<h1>Cart</h1>
+					<h1>Shopping Cart</h1>
 					<h2>Find everything okay?</h2>
 				</div>
 				{
 					itemsInCart
 					?
 						<>
-							<div className="cart-header">
-								<span className="product-header">Product</span>
-								<span className="quantity">Quantity</span>
-								<span className="remove">Remove Item</span>
-								<span className="price">Price</span>
-							</div>
-							{
-								cart &&
-								cart.map(cartItem => {
-									return(
-										<div 
-											className="cart-item"
-											key={ cartItem.itemid } 
-										>
-											<div className="cart-product-info">
-												<img 
-													src={ cartItem.image }
-													alt={`${ cartItem.productName } made by ${ cartItem.manufacturer }`}
-												/>
-												<h2>{ cartItem.productName }</h2>
-											</div>
-											<input
-												id="cart-quantity"
-												className="quantity-input"
-												type="number"
-												name="quantity"
-												value={ cartItem.quantity }
-												min="1"
-												max={ cartItem.available }
-												onChange={(event) =>  dispatch({ 
-													type: 'UPDATE_QUANTITY',
-													payload: {
-														productId: cartItem.itemid,
-														quantity: event.target.value
-													}
-												})}
-											/>
-											<button className="delete-item"
-												onClick={() => handleDeleteItem(cartItem.itemid) }
-											>
-												<i className="far fa-trash-alt"></i>
-											</button>
-											<p className="product-price">{ formatCurrency(cartItem.price * cartItem.quantity) }</p>
-										</div>
-									)
-								}
-							)}
-							<div className="cart-info">
-								<div className="total-quantity">
-									<p>Total Items: { totalQuantity(cart) }</p>
+							<div className="cart-and-info">
+								<div className="cart">
+									<div className="cart-header">
+										<span className="remove">Remove Item</span>
+										<span className="product-header">Product</span>
+										<span className="quantity">Quantity</span>
+										<span className="price">Price</span>
+										<span className="subtotal">SubTotal</span>
+									</div>
+								{
+									cart &&
+										cart.map(cartItem => {
+											return(
+												<div 
+													className="cart-item"
+													key={ cartItem.itemid } 
+												>
+													<button className="delete-item"
+														onClick={() => handleDeleteItem(cartItem.itemid) }
+													>
+														<i className="far fa-trash-alt"></i>
+													</button>
+													<div className="cart-product-info">
+														<img 
+															src={ cartItem.image }
+															alt={`${ cartItem.productName } made by ${ cartItem.manufacturer }`}
+														/>
+														<h2>{ cartItem.productName }</h2>
+													</div>
+													<input
+														id="cart-quantity"
+														className="quantity-input"
+														type="number"
+														name="quantity"
+														value={ cartItem.quantity }
+														min="1"
+														max={ cartItem.available }
+														onChange={(event) =>  dispatch({ 
+															type: 'UPDATE_QUANTITY',
+															payload: {
+																productId: cartItem.itemid,
+																quantity: event.target.value
+															}
+														})}
+													/>
+													<p className="product-price">{ formatCurrency(cartItem.price) }</p>
+													<p className="product-subtotal">{ formatCurrency(cartItem.price * cartItem.quantity) }</p>
+												</div>
+											)
+										}
+								)}
 								</div>
-								<div className="total-price">
-									<p>SubTotal: { formatCurrency(totalPrice(cart)) }</p>
+								<div className="cart-info">
+									<div className="cart-header">
+										<span>Cart Total</span>
+									</div>
+									<div className="total-quantity">
+										<p><span className="bold-text">Total Items</span>:</p><p>{ totalQuantity(cart) }</p>
+									</div>
+									<div className="subtotal-price">
+										<p><span className="bold-text">Subtotal</span>:</p><p>{ formatCurrency(totalPrice(cart)) }</p>
+									</div>
+									<div className="tax">
+										<p><span className="bold-text">Tax</span>:</p><p>Based Off Location</p>
+									</div>
+									<div className="total-price">
+										<p><span className="bold-text">Total</span>:</p><p>{ formatCurrency(totalPrice(cart)) }</p>
+									</div>
+									<div className="cart-actions">
+									<button
+										className="clear-cart"
+										onClick={ () => handleClearCart() }
+									>
+										Empty Cart
+									</button>
+									<button className="checkout">
+										Proceed to Checkout
+									</button>
+								</div>
 								</div>
 							</div>
-							<div className="cart-actions">
-								{/* TODO:// Confirm Modal */}
-								<button
-									className="clear-cart"
-									onClick={ () => handleClearCart() }
-								>
-									Empty Cart
-								</button>
-								<button className="checkout">
-									Proceed to Checkout
-								</button>
-							</div>
+							
 						</>
 					: (
 						<div className="empty-cart">
