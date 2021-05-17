b94a2e3ef6adb8944343198068b9405bcd0120b9
Shugmi Shumunov <SShumunov@gmail.com>
2021-05-16 23:37:03 -0400

    Update 404 copy

--- pages/404.js
+++ pages/404.js
@@ -10,16 +10,16 @@ export default function NotFound(){
 		setTimeout(() => {
 			router.push('/');
 		}, 5000);
-	}, []);
+	}, [router]);
 
 	return(
 		<div className="not-found-container">
 			<div className="page-header">
 				<h1>Page Not Found</h1>
-				<h2>Are you sure you have the right address</h2>
+				<h2>Sure you have the right address?</h2>
 			</div>
 			<div className="not-found-body">
-				<p>Use the links below to navigate to some of our most popular pages</p>
+				<p>Use the links below to navigate to our most popular pages</p>
 				<p>You'll be redirected to the homepage in 5 seconds automatically</p>
 				<ul>
 					Return:
