c3b9da1356d250192480c46f212ae0ec45e07487
Shugmi Shumunov <SShumunov@gmail.com>
2021-05-20 13:31:46 -0400

    Call pageview base off Next router change
    More info on Next Router events: https://nextjs.org/docs/api-reference/next/router#routerevents

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,28 @@
+import { useEffect } from 'react';
 import Layout from '../Components/Layout';
 import { CartProvider } from '../context/CartProvider';
 import { ModalProvider } from '../context/ModalProvider';
+import { useRouter } from 'next/router';
+import { handlePageView } from '../analytics/google';
 
 import '../styles/app.scss';
 
 function MyApp({ Component, pageProps }) {
+	const router = useRouter();
+
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			handlePageView(url);
+		}
+
+		// Next Router Events: https://nextjs.org/docs/api-reference/next/router#routerevents
+		// handle pageview on mount
+		router.events.on('routeChangeComplete', handleRouteChange);
+
+		// cleanup
+		return () => router.events.off('routeChangeComplete', handleRouteChange);
+	}, [router.events]);
+
 	return(
 		<ModalProvider>
 			<CartProvider>
