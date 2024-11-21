dfb80b5b444977030a868d7afc8b1f82df720c9e
Shugmi Shumunov <SShumunov@gmail.com>
2024-11-21 18:50:29 -0500

    fix: Resolve SASS loading issue

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /* config options here */
+  /* config options here */
+  sassOptions: {
+    fibers: false,
+    implementation: 'sass',
+    includePaths: ['./styles/app.scss'],
+    silenceDeprecations: ['legacy-js-api'],
+  },
 };
 
 module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,37 @@
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { handlePageView } from '../analytics/google';
 import Layout from '../Components/Layout';
 import { CartProvider } from '../context/CartProvider';
 import { ModalProvider } from '../context/ModalProvider';
-import { useRouter } from 'next/router';
-import { handlePageView } from '../analytics/google';
 
-// import '../styles/app.scss';
+import '../styles/app.scss';
 
 function MyApp({ Component, pageProps }) {
-	const router = useRouter();
+  const router = useRouter();
 
-	useEffect(() => {
-		const handleRouteChange = (url) => {
-			handlePageView(url);
-		}
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      handlePageView(url);
+    };
 
-		// Next Router Events: https://nextjs.org/docs/api-reference/next/router#routerevents
-		// handle pageview on mount
-		router.events.on('routeChangeComplete', handleRouteChange);
+    // Next Router Events: https://nextjs.org/docs/api-reference/next/router#routerevents
+    // handle pageview on mount
+    router.events.on('routeChangeComplete', handleRouteChange);
 
-		// cleanup
-		return () => router.events.off('routeChangeComplete', handleRouteChange);
-	}, [router.events]);
+    // cleanup
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
+  }, [router.events]);
 
-	return(
-		<ModalProvider>
-			<CartProvider>
-				<Layout>
-					<Component {...pageProps} />
-				</Layout>
-			</CartProvider>
-		</ModalProvider>
-	);
+  return (
+    <ModalProvider>
+      <CartProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </CartProvider>
+    </ModalProvider>
+  );
 }
 
 export default MyApp;
