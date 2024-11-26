dce9cb086b16a36a3dbbc2a9be887ed14339c460
Shugmi Shumunov <SShumunov@gmail.com>
2024-11-26 13:51:41 -0500

    chore: Import styles in custom `Document` & use Next `Script`

--- pages/_document.js
+++ pages/_document.js
@@ -1,42 +1,52 @@
 // More Info On Custom Documents: https://nextjs.org/docs/advanced-features/custom-document
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
-	static async getInitialProps(context) {
-		const initialProps = await Document.getInitialProps(context)
-		return { ...initialProps }
-	}
-	
-	render() {
-		const googleID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+  static async getInitialProps(context) {
+    const initialProps = await Document.getInitialProps(context);
+    return { ...initialProps };
+  }
 
-		return (
-			<Html lang="en">
-				{/* Global Site Tag (gtag.js) - Google Analytics */}
-				<script
-					async
-					src={`https://www.googletagmanager.com/gtag/js?id=${ googleID }`}
-				/>
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
-							window.dataLayer = window.dataLayer || [];
-							function gtag(){dataLayer.push(arguments);}
-							gtag('js', new Date());
-							gtag('config', '${ googleID }', {
-								page_path: window.location.pathname,
-							});
-						`,
-					}}
-				/>
-				<Head />
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		)
-	}
+  render() {
+    const googleID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="UTF-8" />
+          <link
+            rel="stylesheet"
+            href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css"
+          />
+          <link rel="stylesheet" href="../static/normalize.css" />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${googleID}`}
+          />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${googleID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default MyDocument
+export default MyDocument;
