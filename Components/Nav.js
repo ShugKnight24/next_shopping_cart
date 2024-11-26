edf545892c28753e5c77ee3c7658eb86f28b0530
Shugmi Shumunov <SShumunov@gmail.com>
2024-11-26 13:53:33 -0500

    refactor: Shop logo is a link to home & add aria label to nav links

--- Components/Nav.js
+++ Components/Nav.js
@@ -1,27 +1,33 @@
-import { useContext } from 'react';
 import Link from 'next/link';
-import { Logo } from './Logo';
+import { useContext } from 'react';
 import { CartContext } from '../context/CartProvider';
 import { totalQuantity } from '../utils/cartUtils';
+import { Logo } from './Logo';
 
 export default function Nav() {
   const { state } = useContext(CartContext);
-  const { cart } = state;
+  const { cart } = state || { cart: [] };
   const cartCount = totalQuantity(cart) > 99 ? '99+' : totalQuantity(cart);
   const cartIconSize =
     cartCount === '99+' ? 'huge' : cartCount >= 10 ? 'xl' : '';
 
   return (
     <div className="nav-container">
-      <Logo />
+      <Link href="/" aria-label="Home / Shop">
+        <Logo />
+      </Link>
       <nav>
-        <Link href="/">Shop</Link>
-        <Link href="/favorites">Favorites</Link>
-        <Link href="/products">Products</Link>
-        <Link className="cart-nav-item" href="/cart">
-          {/* <a className="cart-nav-item"> */}
+        <Link href="/" aria-label="Home / Shop">
+          Shop
+        </Link>
+        <Link href="/favorites" aria-label="Favorites">
+          Favorites
+        </Link>
+        <Link href="/products" aria-label="Products">
+          Products
+        </Link>
+        <Link className="cart-nav-item" href="/cart" aria-label="Cart">
           <span className={`cart-count ${cartIconSize}`}>{cartCount}</span>
-          {/* </a> */}
         </Link>
       </nav>
     </div>
