475456fcccba6bbd593fe1f01fc8ec72412334f4
Shugmi Shumunov <SShumunov@gmail.com>
2026-09-05 22:52:38 -0400

    fix(analytics): guard Google Tag window.gtag execution against undefined runtime

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 import PropTypes from 'prop-types';
 import { useEffect } from 'react';
-import { handlePageView } from '../analytics/google';
+import { GA_TRACKING_ID, handlePageView } from '../analytics/google';
 import Layout from '../Components/Layout';
 import { ToastProvider } from '../Components/UI/Toast';
 import { CartProvider } from '../context/CartProvider';
@@ -26,15 +27,39 @@ function MyApp({ Component, pageProps }) {
   }, [router.events]);
 
   return (
-    <ToastProvider>
-      <ModalProvider>
-        <CartProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </CartProvider>
-      </ModalProvider>
-    </ToastProvider>
+    <>
+      {GA_TRACKING_ID && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+        </>
+      )}
+      <ToastProvider>
+        <ModalProvider>
+          <CartProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </CartProvider>
+        </ModalProvider>
+      </ToastProvider>
+    </>
   );
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 // More Info On Custom Documents: https://nextjs.org/docs/advanced-features/custom-document
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 class MyDocument extends Document {
   static async getInitialProps(context) {
@@ -9,37 +8,12 @@ class MyDocument extends Document {
   }
 
   render() {
-    const googleID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
-
     return (
       <Html lang="en">
         <Head>
           <meta charSet="UTF-8" />
           <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
           <link rel="alternate icon" href="/favicon.ico" />
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          {googleID && (
-            <>
-              <Script
-                strategy="afterInteractive"
-                src={`https://www.googletagmanager.com/gtag/js?id=${googleID}`}
-              />
-              <Script
-                id="google-analytics"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                  __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${googleID}', {
-                      page_path: window.location.pathname,
-                    });
-                  `,
-                }}
-              />
-            </>
-          )}
         </Head>
         <body>
           <Main />
